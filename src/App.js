c199b3a43967c95b74285588f6346a539fb3841b
EC2 Default User <ec2-user@ip-172-31-16-139.us-east-2.compute.internal>
2019-07-12 15:08:07 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React
+import React, { Component } from 'react';
+import Home from './Home';
+import Navigation from './Navigation'
+import Welcome from './Welcome'
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      user : 'John Doe'
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Navigation user={this.state.user} />
+        {this.state.user && (
+        <Welcome user={this.state.user} />
+        )}
+        <Home user={this.state.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
